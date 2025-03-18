1bd28303c05740ef2db2ed59babd584241468096
Dymytryi <fighttournament2@gmail.com>
2025-03-18 14:18:16 +0200

    share character via messengers

--- app/src/main/java/com/bugbender/gameofthronescharacters/character/presentation/CharacterScreen.kt
+++ app/src/main/java/com/bugbender/gameofthronescharacters/character/presentation/CharacterScreen.kt
@@ -1,7 +1,9 @@
 package com.bugbender.gameofthronescharacters.character.presentation
 
+import android.content.Intent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bugbender.gameofthronescharacters.character.presentation.components.CharacterContent
@@ -13,9 +15,28 @@ fun CharacterScreen() {
     val viewModel = hiltViewModel<CharacterViewModel>()
     val screenState by viewModel.screenState.collectAsStateWithLifecycle()
 
+    val context = LocalContext.current
+    val shareCharacter: (CharacterUi) -> Unit = { character ->
+        val shareText = buildString {
+            appendLine("🔥 Discover ${character.name} from Game of Thrones!")
+            appendLine("Played by: ${character.actor}")
+            appendLine("Debuted in: ${character.debut}")
+            appendLine("\n👉 Want to see more details, photos, and memorable moments?")
+            appendLine("Download the app here:")
+            appendLine("https://play.google.com/store/apps/details?id=com.bugbender.gameofthronescharacters")
+        }
+
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, shareText)
+        }
+        context.startActivity(Intent.createChooser(intent, "Share via"))
+    }
+
     CharacterScreenContent(
         screenState = screenState,
         onRandomIconClick = viewModel::loadRandomCharacter,
+        onShareIconClick = shareCharacter
     )
 }
 
@@ -23,6 +44,7 @@ fun CharacterScreen() {
 fun CharacterScreenContent(
     screenState: CharacterViewModel.ScreenState,
     onRandomIconClick: () -> Unit,
+    onShareIconClick: (CharacterUi) -> Unit
 ) {
     when (screenState) {
         is CharacterViewModel.ScreenState.Loading -> {
@@ -41,7 +63,7 @@ fun CharacterScreenContent(
             CharacterContent(
                 characterUi = screenState.character,
                 onRandomIconClick = onRandomIconClick,
-                onShareIconClick = {},
+                onShareIconClick = { onShareIconClick(screenState.character) },
                 onFavoriteIconClick = {}
             )
         }
