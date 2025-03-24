007d2642b474e4c9253501fa32fe5e54738bcc31
Dymytryi <fighttournament2@gmail.com>
2025-03-24 17:06:38 +0200

    test release 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,9 +13,9 @@ android {
 
     defaultConfig {
         applicationId = "com.bugbender.gameofthronescharacters"
-        minSdk = 29
+        minSdk = 28
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
